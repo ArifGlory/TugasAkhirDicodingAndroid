0bbfb808b83ce21b7cb905ce71d69f7110921b42
Arif Glory <arifglory46@gmail.com>
2024-10-24 15:59:08 +0700

    add detail activity

---
+++ app/src/main/java/com/tapisdev/dicodingandroidtugas1/DetailActivity.kt
@@ -0,0 +1,48 @@
+package com.tapisdev.dicodingandroidtugas1
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.MenuItem
+import com.bumptech.glide.Glide
+import com.tapisdev.dicodingandroidtugas1.databinding.ActivityDetailBinding
+import com.tapisdev.dicodingandroidtugas1.model.Article
+
+class DetailActivity : AppCompatActivity() {
+    lateinit var binding : ActivityDetailBinding
+    lateinit var article: Article
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityDetailBinding.inflate(layoutInflater)
+        val view = binding.root
+        setContentView(view)
+
+        article = intent.getParcelableExtra("article")!!
+
+
+        setupUI()
+    }
+
+    private fun setupUI(){
+        val actionBar = supportActionBar
+        actionBar?.title = "Detail Article"
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
+
+        binding.tvTitle.text = article.title
+        binding.tvDescription.text = article.description
+        Glide.with(this)
+            .load(article.image)
+            .placeholder(R.drawable.placeholder)
+            .into(binding.ivBackdrop)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        return when (item.itemId) {
+            android.R.id.home -> {
+               onBackPressed()
+                true
+            }
+            else -> super.onOptionsItemSelected(item)
+        }
+    }
+}
--- app/src/main/java/com/tapisdev/dicodingandroidtugas1/adapter/ArticleAdapter.kt
+++ app/src/main/java/com/tapisdev/dicodingandroidtugas1/adapter/ArticleAdapter.kt
@@ -1,6 +1,7 @@
 package com.tapisdev.dicodingandroidtugas1.adapter
 
 import android.annotation.SuppressLint
+import android.content.Intent
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
@@ -9,6 +10,7 @@ import android.widget.LinearLayout
 import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
 import com.bumptech.glide.Glide
+import com.tapisdev.dicodingandroidtugas1.DetailActivity
 import com.tapisdev.dicodingandroidtugas1.R
 import com.tapisdev.dicodingandroidtugas1.model.Article
 
@@ -44,7 +46,9 @@ class ArticleAdapter (private var articles : List<Article>) :
             .into(holder.ivArticle)
 
         holder.llArticle.setOnClickListener {
-            //listener.onHistoryClick(history.episodeId)
+            val i = Intent(holder.llArticle.context,DetailActivity::class.java)
+            i.putExtra("article",article)
+            holder.llArticle.context.startActivity(i)
         }
     }
 
